23b3a80f432b091179d4a9e1790e6c623dc14678
Clemencia <clemencia.mora.herrera@cern.ch>
2016-06-24 22:30:22 +0200

    remove from here

--- CondCore/CTPPSPlugins/plugins/plugin.cc
+++
@@ -1,5 +0,0 @@
-#include "CondCore/ESSources/interface/registration_macros.h"
-#include "CondFormats/CTPPSObjects/interface/CTPPSRPPositions.h"
-#include "CondFormats/DataRecord/interface/CTPPSRPPositionsRcd.h"
-
-REGISTER_PLUGIN(CTPPSRPPositionsRcd,CTPPSRPPositions);
