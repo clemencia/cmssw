93cce008096f7700bcf7bf8008d6bf3548fa5521
Jan Kaspar <jan.kaspar@cern.ch>
2018-12-19 15:48:24 +0100

    Clean up after change of code structure.

--- CondFormats/CTPPSReadoutObjects/python/CTPPSRPAlignmentCorrectionsDataESSourceXML_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ctppsRPAlignmentCorrectionsDataESSourceXML = cms.ESSource("CTPPSRPAlignmentCorrectionsDataESSourceXML",
-    verbosity = cms.untracked.uint32(1),
-    XMLFile = cms.string("")
-)
